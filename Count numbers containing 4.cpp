0c023b4ed723fa0bba74cad732010ca6d0cbf71e
yashdar <yashdarji1801@gmail.com>
2024-06-20 16:12:11 -0700

    initial commit

--- Count numbers containing 4.cpp
+++ Count numbers containing 4.cpp
@@ -1,15 +1,20 @@
-class Solution {
-  public:
-    int countNumberswith4(int n) {
+class Solution
+{
+public:
+    int countNumberswith4(int n)
+    {
         // code here
-        int ans=0;
-        for(int i=1; i<=n ; i++){
-            string s= to_string(i);
-            for(int x=0; x<s.length();x++){
-            if(s[x]== '4'){
-                ans++;
-                break;
-            }
+        int ans = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            string s = to_string(i);
+            for (int x = 0; x < s.length(); x++)
+            {
+                if (s[x] == '4')
+                {
+                    ans++;
+                    break;
+                }
             }
         }
         return ans;
